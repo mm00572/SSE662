bf6af3c1cff059d78606080f6ff44916b04db8d9
mm00572 <mm00572@georgiasouthern.edu>
2015-09-09 19:08:19 -0400

    Rename BankTest

--- Bank.Test/Properties/AssemblyInfo.cs
+++ Bank.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bank.Test")]
+[assembly: AssemblyTitle("BankTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bank.Test")]
+[assembly: AssemblyProduct("BankTest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
