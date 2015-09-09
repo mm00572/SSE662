2ad80148bd5ad5c5ff9d30ef0f737126966fc5bd
mm00572 <mm00572@georgiasouthern.edu>
2015-09-08 20:12:17 -0400

    Setup. Begin creating banking application to demonstrate Test Driven Development

---
+++ Bank.Test/BankTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank.Test
+{
+    /// <summary>
+    /// Summary description for BankTest
+    /// </summary>
+    [TestClass]
+    public class BankTest
+    {
+        public BankTest()
+        {
+            Bank bank = new Bank();
+            Assert.IsNotNull(bank);
+
+            Bank bank2 = new Bank("Matthew Mills", 500.25);
+            Assert.IsNotNull(bank);
+            Assert.AreEqual(bank2.owner, "Matthew Mills");
+            Assert.AreEqual(bank2.balance, 500.25);
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
